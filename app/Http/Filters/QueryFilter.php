6b4852fe0eb51dc63a38729592424e549cc99195
HanJie-0523 <onghanjie0523@gmail.com>
2025-11-29 23:03:01 +0800

    chore: add docblock for query filter

--- app/Http/Filters/QueryFilter.php
+++ app/Http/Filters/QueryFilter.php
@@ -6,14 +6,41 @@ use Illuminate\Http\Request;
 
 abstract class QueryFilter
 {
+    /**
+     * The current Eloquent query builder instance.
+     *
+     * @var \Illuminate\Database\Eloquent\Builder
+     */
     protected $query;
+
+    /**
+     * The current HTTP request instance.
+     *
+     * @var \Illuminate\Http\Request
+     */
     protected $request;
 
+    /**
+     * Create a new QueryFilter instance.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     */
     public function __construct(Request $request)
     {
         $this->request = $request;
     }
 
+
+    /**
+     * Apply the filter methods based on request query parameters.
+     *
+     * For each request key, if a method with the same name exists
+     * in the concrete filter class, it will be executed.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
     public function apply($query)
     {
         $this->query = $query;
